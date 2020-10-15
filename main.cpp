4a37eb78e6bb3c1eb92bc40aed477961c23a2c29
INFORMATICS\informatics <peerasakp@gmail.com>
2020-10-15 14:04:06 +0700

    edit hello to hello wolrd

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	cout << "hello";
+	cout << "hello world";
 	return 0;
 }
